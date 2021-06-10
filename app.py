baf0736af27522525f430da8dcb9e9722bd59b9c
Tree <56370416+Tojo-hung@users.noreply.github.com>
2021-06-10 07:20:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
+from flask import request
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/postmethod', methods = ['POST', 'GET'])
+def get_post_javascript_data():
+    jsdata = request.form['direction']
+    return f"<h1>{jsdata}</h1>"
+    print(jsdata)
+
 if __name__ == '__main__':
-   app.run(host="0.0.0.0")
+   app.run(host="0.0.0.0", debug=True)
