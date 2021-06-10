c82aa5fe465c01b6c7ddb068df414f794d5be44c
Tree <56370416+Tojo-hung@users.noreply.github.com>
2021-06-10 07:44:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,34 @@ import json
 
 app = Flask(__name__)
 
+def forward():
+   
+def backward():
+   
+def right():
+   
+def left():
+   
+def stop():
+   
 @app.route('/')
 def home():
    return render_template('index.html')
 
-@app.route('/postmethod', methods = ['POST', 'GET'])
-def get_post_javascript_data():
-    jsdata = request.form['direction']
-    return f"<h1>{jsdata}</h1>"
-    print(jsdata)
+@app.route("/<direction>")
+def action(direction):
+	if direction == 'forward':
+		forward()
+	if direction == 'backward':
+		backward()
+	if direction == 'right':
+		right()
+   if direction == 'left':
+		left()
+	if direction == 'stop':
+		stop()   
+	return render_template('index.html')
+
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True)
